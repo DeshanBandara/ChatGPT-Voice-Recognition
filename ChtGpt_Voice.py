e1433f4f1d9691f85681342ff7aaff6786c25185
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-08-08 15:57:13 +0530

    add extentions and functions

---
+++ ChtGpt_Voice.py
@@ -0,0 +1,44 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+import time
+
+# Set OpenAi API key
+openai.api_key = ""
+
+#initialize the text to speech
+engine = pyttsx3.init()
+
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print("Skipping Unknown error")
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt=prompt,
+        max_tokens=4000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response["choices"][0]["text"]
+
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def main():
+    while True:
+        #Wait for user to say "Genius"
+        print("Say 'Genius' to start recording your question.")
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+            try:
+                transcription = recognizer.recognize_google(audio)
